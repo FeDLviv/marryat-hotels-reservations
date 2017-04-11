e8e99dc3ce07ed462fafdb50b942eb2cbd0ad4ef
Ievgenii Lopushen <lopushen@gmail.com>
2017-04-12 01:45:58 +0300

    initial commit

---
+++ src/main/java/com/marryat/MarryatHotelsReservationsApplication.java
@@ -0,0 +1,12 @@
+package com.marryat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarryatHotelsReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarryatHotelsReservationsApplication.class, args);
+	}
+}
